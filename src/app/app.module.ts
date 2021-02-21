7d92817d4305fbd8ef88bd0311fad401e953e15f
UsamaTariq-1996 <tariqusama16@gmail.com>
2021-02-21 11:30:39 +0500

    functional requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
